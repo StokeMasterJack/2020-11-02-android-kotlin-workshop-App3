ff3aa21891745687eae4d77eb790956e85adad4f
David Ford <dford@smart-soft.com>
2020-11-04 16:11:59 -0800

    Decrease Row Count

--- app/src/main/java/akw/app3/MainActivity.kt
+++ app/src/main/java/akw/app3/MainActivity.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.Text
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
@@ -26,15 +30,35 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
 }
 
 @Composable
 fun ScaffoldDemo() {
     val scaffoldState = rememberScaffoldState()
+    val (rowCount, setRowCount) = remember { mutableStateOf(60) }
     Scaffold(
             scaffoldState = scaffoldState,
-            drawerContent = { Text("Drawer Content") },
+            drawerContent = {
+                Column(modifier = Modifier.padding(all = 20.dp)) {
+                    Text("Drawer Content")
+                    Spacer(modifier = Modifier.preferredHeight(20.dp))
+                    Button(onClick = {
+                        setRowCount(rowCount - 20)
+                        scaffoldState.drawerState.close()
+                    }) {
+                        Text("Reduce Item Count by 20")
+
+                    }
+                    Spacer(modifier = Modifier.preferredHeight(20.dp))
+                    Button(onClick = {
+                        setRowCount(rowCount + 20)
+                        scaffoldState.drawerState.close()
+                    }) {
+                        Text("Increase Item Count by 20")
+                    }
+                }
+
+            },
             topBar = {
                 TopAppBar(
                         title = { Text("abc123") },
@@ -47,8 +71,8 @@ fun ScaffoldDemo() {
                 )
             },
             bodyContent = { innerPadding ->
-                ScrollableColumn(contentPadding = innerPadding) {
-                    repeat(100) { Text("Row $it") }
+                ScrollableColumn(contentPadding = innerPadding,modifier = Modifier.padding(all = 5.dp)) {
+                    repeat(rowCount) { Text("Row ${it+1}", modifier = Modifier.fillMaxWidth()) }
                 }
             },
             floatingActionButton = { ExtendedFloatingActionButton(text = { Text("fab") }, onClick = {}) }
